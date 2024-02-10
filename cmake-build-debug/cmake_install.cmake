06ae7c8ad8623f4e998955d46d22ae5f4749e42a
Hamza Bin Aamir <u2023219@giki.edu.pk>
2024-02-10 21:22:39 +0500

    cmake stuff

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Hamza/CLionProjects/PyPlusPlus
+# Install script for directory: C:/Users/Hamza/Documents/GitHub/HappyBirthdayMyZehru.github.io/py-plus-plus
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Hamza/CLionProjects/PyPlusPlus/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Hamza/Documents/GitHub/HappyBirthdayMyZehru.github.io/py-plus-plus/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
